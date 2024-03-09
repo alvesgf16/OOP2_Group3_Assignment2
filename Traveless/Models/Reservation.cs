422939b0e533359e5abd3042737f18addca1a341
Gabriel Windows <alvesgabriel1994@gmail.com>
2024-03-09 13:34:00 -0700

    create Reservation model

---
+++ Traveless/Models/Reservation.cs
@@ -0,0 +1,33 @@
+﻿namespace Traveless.Models
+{
+    internal class Reservation(Flight reservedFlight, string customerName, string customerCitizenship)
+    {
+        public string Code { get; } = GenerateReservationCode();
+        public Flight ReservedFlight { get; } = reservedFlight;
+        public string CustomerName { get; set; } = customerName;
+        public string CustomerCitizenship { get; set; } = customerCitizenship;
+        public bool Status { get; set; } = true;
+
+        private static string GenerateReservationCode()
+        {
+            Random random = new();
+            string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string numbers = "1234567890";
+            string result = "";
+
+            int randomLetterPosition = random.Next(letters.Length);
+            result += letters[randomLetterPosition];
+            while (result.Length < 5)
+            {
+                int randomNumberPosition = random.Next(numbers.Length);
+                result += numbers[randomNumberPosition];
+            }
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return $"{Code},{ReservedFlight.Code},{CustomerName},{CustomerCitizenship},{Status}";
+        }
+    }
+}
