7d47e3497c3a1a83cb7dbd34ce436d87aec5c150
Gabriel Windows <alvesgabriel1994@gmail.com>
2024-03-10 13:19:50 -0600

    implement happy path for making reservations

--- Traveless/Views/FindReserveFlightsView.xaml.cs
+++ Traveless/Views/FindReserveFlightsView.xaml.cs
@@ -6,24 +6,26 @@ namespace Traveless.Views;
 
 public partial class FindReserveFlightsView : ContentPage
 {
-	public FindReserveFlightsView()
+    private ReservationManager reservationManager = new();
+	
+    public FindReserveFlightsView()
 	{
 		InitializeComponent();
 	}
-
-    private ReservationManager ReservationManager { get; } = new ReservationManager();
-
 
 
     private void FindFlightsButton_Clicked(object sender, EventArgs e)
 	{
 
         //var flights = new ObservableCollection<Flight>(reservationManager.FindFlights(from.Text, to.Text, day.Text));
-        var flights = new ObservableCollection<Flight>(ReservationManager.FindFlights(from.Text, to.Text, day.Text));
+        var flights = new ObservableCollection<Flight>(reservationManager.FindFlights(from.Text, to.Text, day.Text));
         listOfFlights.ItemsSource = flights;
         listOfFlights.SelectedIndex = 0;
     }
 
-        
+    private void ReserveButton_Clicked(object sender, EventArgs e)
+    {
+        Reservation reservation = reservationManager.MakeReservation((Flight)listOfFlights.SelectedItem, name.Text, citizenship.Text);
+        reservationCode.Text = reservation.Code;
     }
 }
